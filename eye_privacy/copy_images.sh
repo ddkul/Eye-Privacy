6c6533d0e9757e311f5557518b87ed6c477333cf
Divya Kulkarni <74252175+ddkul@users.noreply.github.com>
2022-06-14 12:07:32 +0530

    Update copy_images.sh

--- eye_privacy/copy_images.sh
+++ eye_privacy/copy_images.sh
@@ -2,6 +2,6 @@
 
 docker exec -it model_server bash
 
-docker cp model_server:/code/model/eyesave1.png /home/divya/Documents/fastapi-projects/eye_privacy/images
+docker cp model_server:/code/model/eyesave1.png /path/eye_privacy/images
 
-docker cp model_server:/code/model/eyesave2.png /home/divya/Documents/fastapi-projects/eye_privacy/images
+docker cp model_server:/code/model/eyesave2.png /path/eye_privacy/images
